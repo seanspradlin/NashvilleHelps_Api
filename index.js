0b3836a3c14ff86e816df7789385ebc4ebcb8c14
Sean Spradlin <seanspradlin@gmail.com>
2016-10-15 18:34:09 -0500

    configure app

--- index.js
+++ index.js
@@ -2,7 +2,15 @@
 const config = require('./config');
 const express = require('express');
 const winston = require('winston');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(config.port, () => {
   winston.log(`Server is running on port ${config.port}`);
