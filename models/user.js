2de769a09d572ec9b23d411597a09206c9dc3842
Sean Spradlin <seanspradlin@gmail.com>
2016-10-23 10:33:22 -0500

    Added isAdmin to user

--- models/user.js
+++ models/user.js
@@ -14,6 +14,10 @@ const schema = new Schema({
     type: Schema.Types.ObjectId,
     required: true,
   },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 schema.methods.generateHash = function generateHash(password) {
