f29fe3b9208625c5fcc165e9540a9079b6ddcc07
Sean Spradlin <seanspradlin@gmail.com>
2016-10-23 11:45:55 -0500

    include agencies

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const auth = require('./auth');
+const agencies = require('./agencies');
 
 router.use('/auth', auth);
+router.use('/agencies', agencies);
 
 module.exports = router;
 
