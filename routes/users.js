0f11cd093cabef3948a9b6225d5fa1c93505192c
Sean Spradlin <seanspradlin@gmail.com>
2016-12-03 10:54:37 -0600

    allow admins to update users

--- routes/users.js
+++ routes/users.js
@@ -78,6 +78,59 @@ router.get('/:user_id', (req, res) => {
 });
 
 /**
+ * @api {put} /users/:user_id Update a user
+ * @apiName UpdateUser
+ * @apiGroup User
+ *
+ * @apiParam  {String}  [first_name]
+ * @apiParam  {String}  [last_name]
+ * @apiParam  {String}  [email]
+ * @apiParam  {String}  [phone]
+ * @apiParam  {String}  [agency]
+ * @apiParam  {Boolean} [is_admin]
+ *
+ * @apiSuccess  {Object}    users.name
+ * @apiSuccess  {String}    users.name.first
+ * @apiSuccess  {String}    users.name.last
+ * @apiSuccess  {String}    users.email
+ * @apiSuccess  {String}    users.phone
+ * @apiSuccess  {String}    users.agency
+ * @apiSuccess  {Boolean}   users.is_admin
+ *
+ * @apiUse UnauthorizedError
+ */
+router.put('/:user_id', (req, res) => {
+  winston.debug(`PUT /users/${req.params.user_id}`);
+
+  if (!req.isAuthenticated() || !req.user.is_admin) {
+    res.status(401).end();
+  } else {
+    User.findById(req.params.user_id)
+      .then(user => {
+        if (req.body.first_name) user.name.first = req.body.first_name;
+        if (req.body.last_name) user.name.last = req.body.last_name;
+        if (req.body.email) user.email = req.body.email;
+        if (req.body.phone) user.phone = req.body.phone;
+        if (req.body.agency) user.agency = req.body.agency;
+        if (req.body.is_admin) {
+          user.is_admin = typeof req.body.is_admin === 'boolean'
+                        ? req.body.is_admin
+                        : req.body.is_admin === 'true';
+        }
+        return user.save();
+      })
+      .then(user => {
+        res.json(user);
+      })
+      .catch(() => {
+        res
+          .status(400)
+          .json({ error: 'bad request' });
+      });
+  }
+});
+
+/**
  * @api {delete} /users/:user_id Delete a user
  * @apiName DeleteUser
  * @apiGroup User
